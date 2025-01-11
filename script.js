62f698a0d33e75dc0a51b938e295a11b9e0995f2
Clem-Coding <clementine.blondeau@gmail.com>
2025-01-11 13:43:56 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const addButton = document.querySelector(".btn-js");
+
+  const tableBody = document.querySelector("tbody");
+
+  // Ajoutez un écouteur d'événement au bouton
+  addButton.addEventListener("click", function (event) {
+    // Empêche le rechargement de la page lors de l'appui sur le bouton
+    event.preventDefault();
+
+    // Créez une nouvelle ligne
+    const newRow = document.createElement("tr");
+
+    // Créez la première cellule (Nom)
+    const nameCell = document.createElement("td");
+    nameCell.innerHTML = `
+          <div class="mb-3">
+            <label for="name" class="form-label visually-hidden">Nom</label>
+            <input type="text" class="form-control" id="name" aria-describedby="nameHelp" required />
+          </div>
+          `;
+
+    // Créez la deuxième cellule (Email)
+    const emailCell = document.createElement("td");
+    emailCell.innerHTML = `
+          <div class="mb-3">
+            <label for="mail" class="form-label visually-hidden">Email</label>
+            <input type="email" class="form-control" id="mail" aria-describedby="emailHelp" required />
+          </div>
+        `;
+
+    // Ajoutez les cellules à la ligne
+    newRow.appendChild(nameCell);
+    newRow.appendChild(emailCell);
+
+    // Ajoutez la ligne au tableau
+    tableBody.appendChild(newRow);
+  });
+});
